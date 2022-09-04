176583281a37518dc56bea60096468b5f6b5f61b
masakimasui <m.masui2003@gmail.com>
2022-09-05 04:01:17 +0900

    PCの幅でヘッダーの要素に最大3段階のトランジションをつけました。

--- assets/scripts/common.js
+++ assets/scripts/common.js
@@ -3,10 +3,10 @@
 // START HEADER 
 {
   const menuButton = document.getElementById("menu_button");
-  const overlayMenu = document.querySelector(".overlay_navigation");
+  const overlayNavigation = document.querySelector(".overlay_navigation");
 
   menuButton.addEventListener("click", () => {
-    overlayMenu.classList.toggle("show");
+    overlayNavigation.classList.toggle("show");
     console.log('開きました')
   });
 }
